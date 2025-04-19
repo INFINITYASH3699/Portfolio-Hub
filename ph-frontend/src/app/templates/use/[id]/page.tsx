329df5dd06a5229d3f99be27b0f9a0aacd242593
INFINITYASH3699 <infinityash3699@gmail.com>
2025-04-19 19:29:49 +0530

    Uploaded new files

--- ph-frontend/src/app/templates/use/[id]/page.tsx
+++ ph-frontend/src/app/templates/use/[id]/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState, useEffect } from 'react';
-import Image from 'next/image';
+import Image from 'next/image'; 
 import Link from 'next/link';
 import { useRouter, notFound, useParams } from 'next/navigation';
 import { toast } from 'sonner';
